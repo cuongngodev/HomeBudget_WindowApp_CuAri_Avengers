fe62a8ff98494552bba3cefacd3ecb206c7808ea
Avery Berger <averyberger@icloud.com>
2025-03-21 20:23:21 -0400

    updated .ignore to include sqlite3

--- UnitTests/TestHomeBudget.cs
+++ UnitTests/TestHomeBudget.cs
@@ -15,27 +15,27 @@ namespace BudgetCodeTests
 
         // ========================================================================
 
-        //[Fact]
-        //public void HomeBudgetObject_New_NoFileSpecified()
-        //{
-        //    // Arrange
+        [Fact]
+        public void HomeBudgetObject_New_BadFile()
+        {
+            // Arrange
 
-        //    // Act
-        //    HomeBudget homeBudget = new HomeBudget("abc.txt");
+            // Act
+            HomeBudget homeBudget = new HomeBudget("abc.txt");
 
-        //    // Assert 
-        //    Assert.IsType<HomeBudget>(homeBudget);
+            // Assert 
+            Assert.IsType<HomeBudget>(homeBudget);
 
-        //    Assert.True(typeof(HomeBudget).GetProperty("FileName").CanWrite == false, "Filename read only");
-        //    Assert.True(typeof(HomeBudget).GetProperty("DirName").CanWrite == false, "Dirname read only");
-        //    Assert.True(typeof(HomeBudget).GetProperty("PathName").CanWrite == false, "Pathname read only");
-        //    Assert.True(typeof(HomeBudget).GetProperty("categories").CanWrite == false, "categories read only");
-        //    Assert.True(typeof(HomeBudget).GetProperty("expenses").CanWrite == false, "expenses read only");
+            Assert.True(typeof(HomeBudget).GetProperty("FileName").CanWrite == false, "Filename read only");
+            Assert.True(typeof(HomeBudget).GetProperty("DirName").CanWrite == false, "Dirname read only");
+            Assert.True(typeof(HomeBudget).GetProperty("PathName").CanWrite == false, "Pathname read only");
+            Assert.True(typeof(HomeBudget).GetProperty("categories").CanWrite == false, "categories read only");
+            Assert.True(typeof(HomeBudget).GetProperty("expenses").CanWrite == false, "expenses read only");
 
-        //    Assert.Empty(homeBudget.expenses.List());
-        //    Assert.NotEmpty(homeBudget.categories.List());
-        //}
-
+            Assert.Empty(homeBudget.expenses.List());
+            Assert.NotEmpty(homeBudget.categories.List());
+        }
+    
         // ========================================================================
 
         [Fact]
