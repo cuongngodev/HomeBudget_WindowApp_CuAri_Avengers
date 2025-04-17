78448d37a79a769d2a3a3a92ad2c72b9f2fd8d83
Avery Berger <averyberger@icloud.com>
2025-04-17 12:19:56 -0400

    added first version of ViewInterface

---
+++ HomeBudgetApp/HomeBudgetWPF/HomeBudgetWPF/ViewInterface.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Budget;
+namespace HomeBudgetWPF
+{
+    internal interface ViewInterface
+    {
+        public void DisplayError(string message);
+
+        public void DisplayConfirmation(string message);
+
+        public void DisplayCategories(List<Categories> categories);
+
+        public void DisplayCategoryTypes(List<Category.CategoryType> categories);
+
+        
+
+    }
+}
