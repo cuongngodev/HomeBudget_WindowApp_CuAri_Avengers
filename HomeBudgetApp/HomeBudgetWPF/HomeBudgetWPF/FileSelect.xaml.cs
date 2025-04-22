fe3a04a6eacc7ce2e1f9142f4d022813ea4a3f52
Aristedes Patelis <158300078+Entrovos@users.noreply.github.com>
2025-04-22 14:20:27 -0400

    Created codeless structure for File Select, Category, and Expense pages.

---
+++ HomeBudgetApp/HomeBudgetWPF/HomeBudgetWPF/FileSelect.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HomeBudgetWPF
+{
+    /// <summary>
+    /// Interaction logic for FileSelect.xaml
+    /// </summary>
+    public partial class FileSelect : Window
+    {
+        public FileSelect()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectFile(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenDatabase(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
