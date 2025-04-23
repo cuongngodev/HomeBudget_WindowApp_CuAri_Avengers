a97cda7f0a56de48cf35db8e07b81c47e70e89bb
Avery Berger <averyberger@icloud.com>
2025-04-23 18:44:52 -0400

    Fixed and redid viewinterface and Presenter

--- HomeBudgetApp/HomeBudgetWPF/HomeBudgetWPF/Presenter.cs
+++ HomeBudgetApp/HomeBudgetWPF/HomeBudgetWPF/Presenter.cs
@@ -11,12 +11,32 @@ namespace HomeBudgetWPF
     public class Presenter
     {
         private readonly HomeBudget _model;
-        private readonly ViewInterface _view;
+        private readonly ViewInterfaces _view;
 
-        public Presenter(ViewInterface v)
+        public Presenter(ViewInterfaces v)
         {
             _view = v;
-            _model = new HomeBudget();
+            _model = new HomeBudget("../../../../../../HomeBudgetAPI/UnitTests/messyDB.db",false);
         }
+       
+        public List<Category> GetAllCategories()
+        {
+            var boo = _model.categories.List();
+            return boo; 
+        }
+
+        public string GetCategory(int id)
+        {
+            if (id < 0)
+                throw new ArgumentException();
+            if (id > _model.categories.List().Count)
+                throw new ArgumentOutOfRangeException();
+
+            return _model.categories.List()[id].ToString();
+            
+        }
+
+        
+
     }
 }
---
+++ HomeBudgetApp/HomeBudgetWPF/HomeBudgetWPF/ViewInterfaces.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBudgetWPF
+{
+    public class ViewInterfaces
+    {
+        public interface Basic
+        {
+            public void DisplayError(string message);
+
+            public void DisplayConfirmation(string message);
+
+        }
+    }
+}
