4b3989d866ab2e92e36b428d713e1cf1fb897f85
Avery Berger <averyberger@icloud.com>
2025-04-23 12:13:55 -0400

    started presenter

---
+++ HomeBudgetApp/HomeBudgetWPF/HomeBudgetWPF/Presenter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Budget;
+
+namespace HomeBudgetWPF
+{
+  
+    public class Presenter
+    {
+        private readonly HomeBudget _model;
+        private readonly ViewInterface _view;
+
+        public Presenter(ViewInterface v)
+        {
+            _view = v;
+            _model = new HomeBudget();
+        }
+    }
+}
