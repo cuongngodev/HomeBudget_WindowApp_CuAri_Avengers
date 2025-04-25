d8aa4ec31150297f7fe37ca936cabe289a878def
cuongngo <2238147@JAC-F6JH7Y3>
2025-04-25 12:36:05 -0400

    Add MockView class.

--- HomeBudgetApp/HomeBudgetWPF/TestPresenter/TestPresenter/UnitTest1.cs
+++ HomeBudgetApp/HomeBudgetWPF/TestPresenter/TestPresenter/UnitTest1.cs
@@ -2,6 +2,54 @@ namespace TestPresenter;
 using HomeBudgetWPF;
 using Budget;
 
+public class MockView : ViewInterface
+{
+    public bool calledDisplayError;
+    public bool calledDisplayConfirmation;
+    public bool calledDisplayCategoryMenu;
+    public bool calledDisplayExpenseMenu;
+    public bool calledDisplaySelectFileMenu;
+    public bool calledCloseCategoryMenu;
+    public bool calledCloseExpenseMenu;
+    public bool calledCloseFileSelectMenu;
+
+    public void DisplayError(string message)
+    {
+        calledDisplayError = true;
+    }
+
+    public void DisplayConfirmation(string message)
+    {
+        calledDisplayConfirmation = true;
+    }
+
+    public void DisplayCategoryMenu()
+    {
+        calledDisplayCategoryMenu = true;
+    }
+
+    public void DisplayExpenseMenu()
+    {
+        calledDisplayExpenseMenu = true;
+    }
+
+    public void DisplaySelectFileMenu()
+    {
+        calledCloseFileSelectMenu = true;
+    }
+    public void CloseCategoryMenu()
+    {
+        calledCloseCategoryMenu = true;
+    }
+    public void CloseExpenseMenu()
+    {
+        calledCloseExpenseMenu = true;
+    }
+    public void CloseFileSelectMenu()
+    {
+        calledCloseFileSelectMenu = true;
+    }
+}
 public class UnitTest1
 {
     [Fact]
